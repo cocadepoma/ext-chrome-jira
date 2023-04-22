a48c8fdacae8d550d6cc6cf5e77f209f8734fdae
Paco <pacors88@gmail.com>
2023-04-22 11:59:42 +0200

    add dotenv to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,41 +2,17 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
-const app = express();
+import { User } from './schemas';
 
+require('dotenv').config();
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 8000;
+const port = 8001;
 
 mongoose.connect(process.env.MONGO_URI);
-
-const ticketSchema = new mongoose.Schema({
-  _id: String,
-  description: String,
-  createdAt: Number,
-  categoryId: String,
-  content: String,
-  color: String,
-});
-
-const categorySchema = new mongoose.Schema({
-  _id: String,
-  name: String,
-  createdAt: Number,
-  indexOrder: Number,
-  color: String,
-  tickets: [ticketSchema],
-});
-
-const userSchema = new mongoose.Schema({
-  uid: String,
-  email: String,
-  googleId: String,
-  boards: [categorySchema],
-});
-
-const User = mongoose.model('User', userSchema);
 
 app.post('/api/users', async (req: Request, res: Response) => {
   const { email = '', id = '' } = req.body;
@@ -69,27 +45,9 @@ app.post('/api/users', async (req: Request, res: Response) => {
   }
 });
 
-const a2 = {
-  "_id": "afc58449-15e6-4278-986c-6566b2084934",
-  "color": "#c1caf3",
-  "createdAt": 1682152770781,
-  "indexOrder": 0,
-  "name": "dasdasd",
-  "tickets": [
-    {
-      "_id": "267b85b3-01ef-4590-ac38-184fc8e907f1",
-      "categoryId": "afc58449-15e6-4278-986c-6566b2084934",
-      "color": "",
-      "content": "",
-      "createdAt": 1682152774480,
-      "description": "fasfasf"
-    }
-  ]
-};
-
 app.post('/api/users/:uid', async (req: Request, res: Response) => {
   const { uid } = req.params;
-  const { boards = a2 } = req.body;
+  const { boards } = req.body;
 
   try {
     const updatedUser = await User.findOneAndUpdate(
@@ -112,6 +70,3 @@ app.post('/api/users/:uid', async (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`);
 });
-
-// kanbanify-api
-// E7zgB1HsnCWsXrT2
---
+++ backend/src/schemas.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const ticketSchema = new mongoose.Schema({
+  _id: String,
+  description: String,
+  createdAt: Number,
+  categoryId: String,
+  content: String,
+  color: String,
+});
+
+const categorySchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+  createdAt: Number,
+  indexOrder: Number,
+  color: String,
+  tickets: [ticketSchema],
+});
+
+const userSchema = new mongoose.Schema({
+  uid: String,
+  email: String,
+  googleId: String,
+  boards: [categorySchema],
+});
+
+export const User = mongoose.model('User', userSchema);
